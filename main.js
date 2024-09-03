8dc05a3886de796c28b6789c75021e2fde299569
drinyyy <drinylajqi@gmail.com>
2024-09-03 12:22:34 +0200

    vfinalfinal

--- main.js
+++ main.js
@@ -2,143 +2,19 @@ import './style.css';
 import Experience from './Experience/experience';
 import gsap from 'gsap';
 const experience = new Experience(document.querySelector('.experience-canvas'));
-// const dot = document.getElementById('dot');
-// const curve = document.getElementById('curve');
 
-// dot.addEventListener('mouseover', () => {
-//   gsap.to(dot, {
-//     duration: 0.2,
-//     attr: { r: 3 }, // Increase radius to 3 on hover
-//     ease: 'power1.out',
-//   });
-// });
-
-// dot.addEventListener('mouseout', () => {
-//   if (!isDragging) { // Ensure it only reverts when not dragging
-//     gsap.to(dot, {
-//       duration: 0.2,
-//       attr: { r: 2 }, // Revert radius to 2
-//       ease: 'power1.out',
-//     });
-//   }
-// });
-
-// let isDragging = false; // Flag to track if dragging is in progress
-
-// function getMousePosition(evt) {
-//   const CTM = curve.getScreenCTM();
-//   return {
-//     x: (evt.clientX - CTM.e) / CTM.a,
-//     y: (evt.clientY - CTM.f) / CTM.d,
-//   };
-// }
-
-// function getTouchPosition(evt) {
-//   const CTM = curve.getScreenCTM();
-//   const touch = evt.touches[0];
-//   return {
-//     x: (touch.clientX - CTM.e) / CTM.a,
-//     y: (touch.clientY - CTM.f) / CTM.d,
-//   };
-// }
-
-// function distance(point1, point2) {
-//   return Math.sqrt((point1.x - point2.x) ** 2 + (point1.y - point2.y) ** 2);
-// }
-
-// function getClosestPointOnPath(path, point) {
-//   const pathLength = path.getTotalLength();
-//   let closestPoint = path.getPointAtLength(0);
-//   let closestDistance = distance(point, closestPoint);
-
-//   for (let i = 0; i <= pathLength; i++) {
-//     const currentPoint = path.getPointAtLength(i);
-//     const currentDistance = distance(point, currentPoint);
-//     if (currentDistance < closestDistance) {
-//       closestDistance = currentDistance;
-//       closestPoint = currentPoint;
-//     }
-//   }
-//   return closestPoint;
-// }
-
-// function moveDot(evt) {
-//   let mousePos;
-//   if (evt.type === 'mousemove') {
-//     mousePos = getMousePosition(evt);
-//   } else if (evt.type === 'touchmove') {
-//     mousePos = getTouchPosition(evt);
-//   }
-
-//   const closestPoint = getClosestPointOnPath(curve, mousePos);
-//   const pathLength = curve.getTotalLength();
-//   let closestLength = 0;
-//   let closestDistance = distance(mousePos, curve.getPointAtLength(0));
-
-//   for (let i = 0; i <= pathLength; i++) {
-//     const currentPoint = curve.getPointAtLength(i);
-//     const currentDistance = distance(mousePos, currentPoint);
-//     if (currentDistance < closestDistance) {
-//       closestDistance = currentDistance;
-//       closestLength = i;
-//     }
-//   }
-
-//   const normalizedPosition = closestLength / pathLength;
-//   experience.camera.updateCameraPosition(normalizedPosition);
-
-//   gsap.to(dot, {
-//     attr: { cx: closestPoint.x, cy: closestPoint.y },
-//     duration: 0.1, // Animation duration
-//     ease: "power2.out",
-//     delay: 0.1 // Delay before the animation starts
-//   });
-// }
-
-// function onDragStart(evt) {
-//   evt.preventDefault();
-//   isDragging = true;
-
-//   gsap.to(dot, {
-//     duration: 0.2,
-//     attr: { r: 3 }, // Increase radius during drag
-//     ease: 'power1.out',
-//   });
-
-//   document.addEventListener('mousemove', moveDot);
-//   document.addEventListener('touchmove', moveDot);
-// }
-
-// function onDragEnd() {
-//   isDragging = false;
-
-//   gsap.to(dot, {
-//     duration: 0.2,
-//     attr: { r: 2 }, // Revert radius back after dragging ends
-//     ease: 'power1.out',
-//   });
-
-//   document.removeEventListener('mousemove', moveDot);
-//   document.removeEventListener('touchmove', moveDot);
-// }
-
-// dot.addEventListener('mousedown', onDragStart);
-// dot.addEventListener('touchstart', onDragStart);
-// document.addEventListener('mouseup', onDragEnd);
-// document.addEventListener('touchend', onDragEnd);
 document.addEventListener('DOMContentLoaded', function () {
-    // Create and insert the SoundCloud iframe
+    
     function decodeUrl(encodedUrl) {
         return atob(encodedUrl);
     }
 
-    // Obfuscated SoundCloud URL (Base64 encoded)
+    
     var encodedUrl = 'aHR0cHM6Ly9zb3VuZGNsb3VkLmNvbS9kcmluLWxhamNpL3NldHMvY2hpbGxzdGF0aW9u';
     
-    // Decode the URL
+    
     var decodedUrl = decodeUrl(encodedUrl);
 
-    // Create and insert the SoundCloud iframe
     var soundcloudPlayerContainer = document.getElementById('soundcloud-player-container');
     var iframe = document.createElement('iframe');
     iframe.id = 'soundcloud-player';
@@ -150,10 +26,10 @@ document.addEventListener('DOMContentLoaded', function () {
     iframe.src = 'https://w.soundcloud.com/player/?url=' + encodeURIComponent(decodedUrl) + '&auto_play=false&hide_related=false&show_comments=false&show_user=false&show_reposts=false&visual=false';
     soundcloudPlayerContainer.appendChild(iframe);
 
-    // Initialize the SoundCloud player
+    
     var widget = SC.Widget(iframe);
 
-    // Get DOM elements for controls
+    
     var playPauseButton = document.getElementById('playPauseButton');
     var prevButton = document.getElementById('prevButton');
     var nextButton = document.getElementById('nextButton');
@@ -162,13 +38,13 @@ document.addEventListener('DOMContentLoaded', function () {
     var backgroundImageDisplay = document.getElementById('backgroundImage');
     var playPauseIcon = playPauseButton.querySelector('.material-icons');
 
-    // Set initial state
+    
     var isPlaying = false;
     var currentRotation = 0;
     var rotationSpeed = 0.05;
     var animationFrameId;
 
-    // Function to rotate the image
+    
     function rotateImage() {
         if (isPlaying) {
             currentRotation += rotationSpeed;
@@ -189,7 +65,7 @@ document.addEventListener('DOMContentLoaded', function () {
         cancelAnimationFrame(animationFrameId);
     }
 
-    // Play/Pause button functionality
+    
     playPauseButton.addEventListener('click', function () {
         widget.isPaused(function (isPaused) {
             if (isPaused) {
@@ -204,43 +80,43 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     });
 
-    // Next button functionality
+    
     nextButton.addEventListener('click', function () {
         stopRotation();
         widget.next();
         startRotation();
     });
 
-    // Previous button functionality
+    
     prevButton.addEventListener('click', function () {
         stopRotation();
         widget.prev();
         startRotation();
     });
 
-    // Function to update track information
+    
     function updateTrackInfo(currentSound) {
         trackNameDisplay.textContent = currentSound.title;
 
-        // Set track and background images if available
+        
         if (currentSound.artwork_url) {
             var artworkUrl = currentSound.artwork_url.replace('-large', '-t500x500');
             trackImageDisplay.src = artworkUrl;
             backgroundImageDisplay.src = artworkUrl;
         } else {
-            trackImageDisplay.src = ''; // Set to a default image or blank if no artwork
+            trackImageDisplay.src = ''; 
             backgroundImageDisplay.src = '';
         }
     }
 
-    // Update track information on player ready
+   
     widget.bind(SC.Widget.Events.READY, function () {
         widget.getCurrentSound(function (currentSound) {
             updateTrackInfo(currentSound);
         });
     });
 
-    // Update track information on track change
+    
     widget.bind(SC.Widget.Events.PLAY, function () {
         widget.getCurrentSound(function (currentSound) {
             updateTrackInfo(currentSound);
@@ -256,4 +132,58 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
 
-        
+const aboutIcon = document.getElementById('aboutIcon');
+const aboutContainer = document.getElementById('aboutContainer');
+
+
+const tl = gsap.timeline({ paused: true });
+
+
+tl.fromTo(
+  aboutContainer,
+  {
+    scale: 0.8,   
+    opacity: 0,   
+    display: 'none', 
+  },
+  {
+    scale: 1,     
+    opacity: 1,   
+    display: 'flex',
+    duration: 0.5, 
+    ease: 'power2.out', 
+  }
+);
+
+
+aboutIcon.addEventListener('click', () => {
+  if (aboutContainer.classList.contains('show')) {
+    
+    gsap.to(aboutContainer, {
+      scale: 0.8,
+      opacity: 0,
+      duration: 0.5,
+      ease: 'power2.in',
+      onComplete: () => {
+        aboutContainer.style.display = 'none'; 
+        aboutContainer.classList.remove('show'); 
+      }
+    });
+  } else {
+    
+    aboutContainer.style.display = 'flex'; 
+    aboutContainer.classList.add('show'); 
+    tl.restart(); 
+  }
+});
+
+
+document.addEventListener('mousemove', (event) => {
+  const x = (window.innerWidth / 2 - event.clientX) / 50;
+  const y = (window.innerHeight / 2 - event.clientY) / 50;
+  aboutContainer.style.transform = `translate(-50%, -50%) rotateX(${y}deg) rotateY(${x}deg)`;
+});
+     
+
+
+      
